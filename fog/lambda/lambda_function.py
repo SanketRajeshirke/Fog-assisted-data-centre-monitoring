b19aea46316f644b2389eaaf548c40214731dade
Ubuntu <ubuntu@ip-172-31-6-179.ec2.internal>
2026-07-07 12:45:32 +0000

    Fix Lambda DynamoDB Decimal conversion

--- fog/lambda/lambda_function.py
+++ fog/lambda/lambda_function.py
@@ -1,8 +1,11 @@
 import json
 import boto3
+from decimal import Decimal
 
 dynamodb = boto3.resource("dynamodb")
+
 table = dynamodb.Table("SensorData")
+
 
 def lambda_handler(event, context):
 
@@ -10,32 +13,58 @@ def lambda_handler(event, context):
 
     processed = []
 
-    for record in event['Records']:
-        body = json.loads(record['body'])
+    for record in event["Records"]:
 
-        # each batch from fog node
+        body = json.loads(record["body"])
+
+        # batch received from SQS
         for item in body:
 
+            value = item["value"]
+
+            # DynamoDB does not support float
+            if isinstance(value, float):
+                value = Decimal(str(value))
+
+
             enriched = {
+
                 "sensor_id": item["sensor_id"],
+
                 "type": item["type"],
-                "value": item["value"],
+
+                "value": value,
+
                 "timestamp": item["timestamp"],
+
                 "processed_by": "lambda",
+
                 "anomaly": item.get("anomaly", False)
+
             }
+
 
             processed.append(enriched)
 
             print("Processed:", enriched)
 
-            # ✅ STORE IN DYNAMODB
-            table.put_item(Item=enriched)
+
+            # Store in DynamoDB
+            table.put_item(
+                Item=enriched
+            )
+
 
     return {
+
         "statusCode": 200,
+
         "body": json.dumps({
+
             "message": "Data processed successfully",
+
             "count": len(processed)
+
         })
+
     }
